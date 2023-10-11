44fc4765d5bb5db4265c1f82327369583d7fb1f5
Mounir Khalil <mmk113@mail.aub.edu>
2023-10-11 15:28:18 +0300

    after testing rest api

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 import sqlite3
+from flask import Flask, request, jsonify #added to top of file
+from flask_cors import CORS #added to top of file
+
+
 def connect_to_db():
     conn = sqlite3.connect('database.db')
     return conn
@@ -52,14 +56,14 @@ def get_users():
         rows = cur.fetchall()
         # convert row objects to dictionary
         for i in rows:
-        user = {}
-        user["user_id"] = i["user_id"]
-        user["name"] = i["name"]
-        user["email"] = i["email"]
-        user["phone"] = i["phone"]
-        user["address"] = i["address"]
-        user["country"] = i["country"]
-        users.append(user)
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
     except:
         users = []
     return users
@@ -123,3 +127,28 @@ def delete_user(user_id):
 
 
 
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+@app.route('/api/users', methods=['GET'])
+def api_get_users():
+    return jsonify(get_users())
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+
+def api_get_user(user_id):
+    return jsonify(get_user_by_id(user_id))
+@app.route('/api/users/add', methods = ['POST'])
+def api_add_user():
+    user = request.get_json()
+    return jsonify(insert_user(user))
+@app.route('/api/users/update', methods = ['PUT'])
+def api_update_user():
+    user = request.get_json()
+    return jsonify(update_user(user))
+@app.route('/api/users/delete/<user_id>', methods = ['DELETE'])
+def api_delete_user(user_id):
+    return jsonify(delete_user(user_id))
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(debug=True)
+    app.run() #run app
